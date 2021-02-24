00d102272d4a381420642eb165183919f8f85299
Leonardo Pinho <contato@leonardopinho.com>
2021-02-24 08:44:21 -0300

    rafactor

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from src.ytb_utils import YtbUtils
 from src.stream_utils import StreamUtils
 
 
-class YtbDownloadTest(unittest.TestCase):
+class YtbUtilsTest(unittest.TestCase):
     urls = [
         'https://www.youtube.com/watch?v=c_17jALtLbQ',
     ]
